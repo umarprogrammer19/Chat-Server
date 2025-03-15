2894a1b66bc5491bd2bad9ad16a13d5fd530bba5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 21:53:38 +0500

    Error Middleware

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,11 +1,9 @@
+import { errorHandler } from "../utilities/errorHandlers.js";
+
 const registerUser = async (req, res) => {
     try {
         const { fullname, username, password, gender } = req.body;
-        if (!fullname || !username || !password || !gender) return res.status(400).json({
-            success: false,
-            message: "All Fields Are Required"
-        });
+        if (!fullname || !username || !password || !gender) return next(new errorHandler("All Fields Are Required", 400));
     } catch (error) {
-
     }
 }
