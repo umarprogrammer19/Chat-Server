2896290577a6c56b00957a7432a81032f93eb80b
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 16:05:53 +0500

    Basic Server Initialized

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import "dotenv/config";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.send("Chat Application"));
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on the port ${process.env.PORT || 5000}`);
