fddf23913fa1da062d9128cc344191cfd351f799
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 20:32:06 +0500

    Update

--- db/db.connection.js
+++ db/db.connection.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const instance = await mongoose.connect(process.env.MONGODB_URL);
+        const instance = await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: "Chat-Application",
+        });
         console.log(`MongoDB Connected: ${instance.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
