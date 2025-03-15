52965a1a8e81959536e763fde939d7eb3a9fb2d7
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-15 19:53:45 +0500

    User Models

---
+++ models/user.models.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
