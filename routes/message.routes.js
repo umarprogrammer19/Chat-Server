6b0b7faec35ec56f4ef2c8e513a2d4e7d6c5161d
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-16 17:03:57 +0500

    router

---
+++ routes/message.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import isAuthnticated from "../middlewares/auth.middleware.js";
+import { sendMessage } from "../controllers/message.controllers.js";
+
+const router = Router();
+
+router.post("/send", isAuthnticated, sendMessage);
+
+export default router;
