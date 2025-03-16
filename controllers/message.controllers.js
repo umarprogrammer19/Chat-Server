05f0bdf57dd7141bbcd16e0e81b146ab04605a75
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-16 17:01:50 +0500

    Send Message

---
+++ controllers/message.controllers.js
@@ -0,0 +1,42 @@
+import Message from "../models/message.models.js";
+import Conversation from "../models/conversation.models.js";
+import asyncHandler from "../utilities/asyncHandlers.js";
+
+export const sendMessage = asyncHandler(async (req, res, next) => {
+    if (!req.user) return next(new ErrorHandler("Unauthorized", 401));
+
+    const senderId = req.user._id;
+
+    if (!senderId) return next(new ErrorHandler("User Not Found", 404));
+
+    const receiverId = req.params.receiverId;
+    const { message } = req.body;
+
+    if (!receiverId || !message) return next(new ErrorHandler("Incomplete Data", 400));
+
+    let conversation = Conversation.findOne({
+        participants: { $all: { senderId, receiverId } },
+    });
+
+    if (!conversation) {
+        conversation = await Conversation.create({
+            participants: [senderId, receiverId],
+        });
+    };
+
+    const newMessage = await Message.create({
+        senderId,
+        receiverId,
+        message
+    });
+
+    if (newMessage) {
+        conversation.message.push(newMessage._id);
+        await conversation.save();
+    };
+
+    res.status(201).json({
+        sucess: true,
+        newMessage,
+    });
+});
