920c02a0ebb4b3185ce8c877db90111b50ba4708
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-03-18 06:28:42 +0500

    Cookies Saved

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -77,11 +77,12 @@ export const loginUser = asyncHandler(async (req, res, next) => {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: 'None'
+        sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     }).json({
         success: true,
         message: "Login Successfull",
-        token
+        token,
+        user,
     })
 });
 
